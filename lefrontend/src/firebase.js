62e7cbe709c2625facf83e49282a81588376217b
xpectro93 <jonathanandrade@pursuit.org>
2020-01-17 00:43:09 -0500

    add secret.json

--- lefrontend/src/firebase.js
+++ lefrontend/src/firebase.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+const secret = require('./secret.json')
 
 const config = {
-  apiKey: "AIzaSyDnX-V1IEQDlgNv1XwraQA9jXi3pE8Yomk",
+  apiKey: secret.apiKey,
   authDomain: "dumb-uploader.firebaseapp.com",
   databaseURL: "https://dumb-uploader.firebaseio.com",
   projectId: "dumb-uploader",
